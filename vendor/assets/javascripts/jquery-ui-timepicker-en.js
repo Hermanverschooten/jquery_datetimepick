890ccd9c6ae7aa639aa07236d4afa4d1a16bdd1e
Arnaud Kleinveld <arnaud.kleinveld@ybo.com.sg>
2014-06-08 12:35:50 +0700

    Add locale en

---
+++ vendor/assets/javascripts/jquery-ui-timepicker-en.js
@@ -0,0 +1,21 @@
+/* English translation for the jQuery Timepicker Addon */
+/* Written by Arnaud Kleinveld */
+(function($) {
+        $.timepicker.regional['en'] = {
+                timeOnlyTitle: 'Select Time',
+                timeText: 'Time',
+                hourText: 'Hour',
+                minuteText: 'Minute',
+                secondText: 'Second',
+                millisecText: 'Millisecond',
+                microsecText: 'Microsecond',
+                timezoneText: 'Timezone',
+                currentText: 'Today',
+                closeText: 'Close',
+                timeFormat: 'HH:mm',
+                amNames: ['am', 'AM', 'A'],
+                pmNames: ['pm', 'PM', 'P'],
+                isRTL: false
+        };
+        $.timepicker.setDefaults($.timepicker.regional['en']);
+})(jQuery);
